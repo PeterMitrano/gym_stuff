e575d09e7e85e2bda8c0ecd079c235deb022e45d
Peter Mitrano <mitranopeter@gmail.com>
2016-12-23 01:11:18 -0500

    cleaned up and commeneted hillcimbing.py

--- cart_pole/hillclimbing.py
+++ cart_pole/hillclimbing.py
@@ -1,39 +1,63 @@
 #!/usr/bin/python3
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import Figure
 import gym
 import numpy as np
 
-def run_episode(env, parameters):
-	observation = env.reset()
-	totalreward = 0
-	for _ in xrange(1000):
-		env.render()
-		#initalize random weights
-		action = 0 if np.matmul(parameters, observation)<0 else 1
-		observation, reward, done, info = env.step(action)
-		totalreward += reward
-		if done:
-			break
-	return totalreward
+class HillClimbing:
 
-#hill climbing algo training
-def train(submit):
-	env = gym.make('CartPole-v0')
+    def run_episode(self, env, parameters):
+        observation = env.reset()
+        total_reward = 0
+        for _ in range(200):
+            env.render()
+            # compute linear combination of parameters and observations
+            if np.matmul(parameters, observation) < 0:
+                action = 0
+            else:
+                action = 1
+            observation, reward, done, info = env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return total_reward
 
-	episodes_per_update = 5
-	noise_scaling = 0.1
-	parameters = np.random.rand(4) * 2 -1
-	bestreward = 0
+    def train(self):
+        env = gym.make('CartPole-v0')
 
-	#2000 episodes
-	for _ in xrange(2000):
-		newparams = parameters + (np.random.rand(4) *2 -1) * noise_scaling
-		reward = run_episode(env, newparams)
-		print "reward %d best %d" % (reward, bestreward)
-		if reward > bestreward:
-			bestreward = reward
-			parameters = newparams
-			if reward == 200:
-				break
+        # parameters are 4 random values between -1 and 1
+        parameters = np.random.rand(4) * 2 - 1
+        best_reward = 0
 
-r = train(submit=False)
-print r
+        # plotting stuff
+        steps_between_plot = 40
+        plt.ion()
+
+        # 2000 episodes
+        rewards = []
+        for i in range(2000):
+            noise_scaling = 0.05
+            new_params = parameters + (np.random.rand(4) * 2 - 1) * noise_scaling
+            reward = self.run_episode(env, new_params)
+            rewards.append(reward)
+
+            if i % steps_between_plot == 0:
+                plt.plot(rewards)
+                plt.pause(0.05)
+            print("[%i] episode reward %d, best so far %d" % (i, reward, best_reward))
+            if reward > best_reward:
+                best_reward = reward
+                parameters = new_params
+                if reward == 200:
+                    print("game has been solved!")
+                    break
+
+        plt.savefig('fig.png')
+        input("press enter to exit")
+        return best_reward
+
+
+if __name__ == "__main__":
+    hc = HillClimbing()
+    r = hc.train()
+    print(r)
--- cart_pole/random_cart.py
+++ cart_pole/random_cart.py
@@ -3,12 +3,12 @@ import gym
 env = gym.make('CartPole-v0')
 env.reset()
 
-print "angle, reward"
+print("angle, reward")
 for _ in range(1000):
     env.render()
     observations, reward, done, info = env.step(env.action_space.sample()) # take a random action
-    print observations[2], "," , reward
+    print(observations[2], "," , reward)
 
     if done:
-        print "done!"
+        print("done!")
         break
