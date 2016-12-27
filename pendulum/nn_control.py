bcb47964c730eee551eaf289be12eb218c464667
Peter Mitrano <mitranopeter@gmail.com>
2016-12-27 03:27:55 -0500

    nn control works, but not as well as manual control
    also it's still not improving itself

--- pendulum/nn_control.py
+++ pendulum/nn_control.py
@@ -1,91 +1,62 @@
 #!/usr/bin/python3
 from mpl_toolkits.mplot3d import Axes3D
+import time
 import tensorflow as tf
-import manual_control
 import matplotlib.pyplot as plt
 from math import cos, sin
 import os
 import sys
 import gym
 import numpy as np
+import manual_control
 
 
-class ManualControl:
-
-    @staticmethod
-    def policy(observation):
-        theta = np.arctan2(observation[1], observation[0])
-        dtheta = observation[2]
-
-        if abs(theta) > 0.2:  # gather momentum until we're close
-            if (dtheta > 0 and dtheta < 3) and (theta > 0 or theta < -np.pi*2/3):
-                action = 1.5
-            elif (dtheta < 0 and dtheta > -3) and (theta < 0 or theta > np.pi*2/3):
-                action = -1.5
-            else:
-                action = 0
-        else:
-            if theta > 0 and dtheta > 0:
-                action = -2
-            elif theta < 0 and dtheta < 0:
-                action = 2
-            else:
-                action = 0
-
-        return action
+class NNControl:
 
     @staticmethod
-    def run_episode(env, train_iter, render=False):
+    def run_episode(sess, env, action_pred, X, render=False):
         observation = env.reset()
         total_reward = 0
         rewards = []
-        # for _ in range(400):
-        while True:
+        for _ in range(800):
             if render:
                 env.render()
 
-            action = ManualControl.policy(observation)
+            observation = np.reshape(observation, (1, 3))
+            action = sess.run(action_pred, feed_dict={X: observation})
 
             # step the environment
             observation, reward, done, info = env.step([action])
 
             total_reward += reward
             rewards.append(reward)
-            rewards = rewards[-20:]
 
             avg_reward = sum(rewards) / len(rewards)
-            if avg_reward < 0.02:
+            if avg_reward > -0.01:
                 break
 
         return total_reward
 
-    def train(self, show_plot=False, upload=False):
+    @staticmethod
+    def run(sess, action_pred, X, upload=False):
         env = gym.make('Pendulum-v0')
 
         best_reward = -sys.maxsize
 
-        # plotting stuff
-        steps_between_plot = 200
-        plt.ion()
-
+        tag = ''
         if upload:
             tag = '/tmp/' + os.path.basename(__file__) + '-' + str(int(np.random.rand() * 1000))
             env.monitor.start(tag)
 
-        # simple Q learner
         rewards = []
-        max_trials = 200
+        max_trials = 2000
         print_step = 1
         avg_reward = 0
         print('step, rewards, best_reward, 100_episode_avg_reward')
         for i in range(max_trials):
 
-            reward = ManualControl.run_episode(env, i)
+            reward = NNControl.run_episode(sess, env, action_pred, X, render=True)
             rewards.append(reward)
-
-            if show_plot and i % steps_between_plot == 0:
-                plt.plot(rewards)
-                plt.pause(0.05)
 
             if i % print_step == 0:
                 print("%i, %d, %d, %f" % (i, reward, best_reward, avg_reward))
@@ -101,12 +72,37 @@ class ManualControl:
             env.monitor.close()
             gym.upload(tag, api_key='sk_8MyNtnorQEeNtKpCwk2S8g')
 
-        np.savetxt('rewards.csv', rewards, delimiter=',')
+    @staticmethod
+    def test(sess, training_costs, action_pred, X):
+        xs = []
+        ys = []
+        zs = []
+        pred_zs = []
+        for _ in range(2000):
+            theta = np.random.uniform(-np.pi, np.pi)
+            dtheta = np.random.uniform(-8, 8)
+            obs = [cos(theta), sin(theta), dtheta]
+            action = manual_control.policy(obs)
+            obs_arr = np.array([obs])
 
+            xs.append(theta)
+            ys.append(dtheta)
 
-def main():
-    hc = ManualControl()
+            pred_z = sess.run(action_pred, feed_dict={X: obs_arr})[0][0]
 
+            pred_zs.append(pred_z)
+            zs.append(action)
+
+        action_fig = plt.figure(1)
+        training_fig = plt.figure(2)
+        training_ax = training_fig.add_subplot(111)
+        training_ax.plot(training_costs)
+        ax = action_fig.add_subplot(111, projection='3d')
+        ax.scatter(xs, ys, zs=pred_zs, c='b', label='pred')
+        ax.scatter(xs, ys, zs=zs, c='r', label='true')
+        plt.show()
+
+def main(upload=False):
     n_hidden_1 = 16
     n_hidden_2 = 16
 
@@ -123,6 +119,7 @@ def main():
         b2 = tf.Variable(np.random.normal(size=[1, n_hidden_2]), dtype=np.float32)
         fc2 = tf.matmul(fc1, W2)
         fc2 = tf.add(fc2, b2)
+        fc2 = tf.nn.sigmoid(fc2)
 
     with tf.name_scope("h2"):
         W3 = tf.Variable(np.random.normal(size=[n_hidden_2, 1]), dtype=np.float32)
@@ -149,12 +146,13 @@ def main():
 
         sess.run(tf.initialize_all_variables())
 
-        N = 1000
+        # the training phase
+        n = 20000
         avg_costs = []
-        print("cost")
-        for i in range(200):
+        print("step, cost")
+        for i in range(20):
             costs = 0
-            for _ in range(N):
+            for _ in range(n):
                 theta = np.random.uniform(-np.pi, np.pi)
                 dtheta = np.random.uniform(-8, 8)
                 obs = [cos(theta), sin(theta), dtheta]
@@ -167,39 +165,15 @@ def main():
                 costs += c
                 writer.add_summary(s)
 
-            avg_cost = costs/N
-            print(avg_cost)
-            avg_costs.append(costs/N)
+            avg_cost = costs/n
+            print(i, avg_cost)
+            avg_costs.append(costs/n)
 
-        # test!
-        xs = []
-        ys = []
-        zs = []
-        pred_zs = []
-        for _ in range(2000):
-            theta = np.random.uniform(-np.pi, np.pi)
-            dtheta = np.random.uniform(-8, 8)
-            obs = [cos(theta), sin(theta), dtheta]
-            action = hc.policy(obs)
-            obs_arr = np.array([obs])
+        print("Training is complete.")
 
-            xs.append(theta)
-            ys.append(dtheta)
-
-            pred_z = sess.run(action_pred, feed_dict={X: obs_arr})[0][0]
-
-            pred_zs.append(pred_z)
-            zs.append(action)
-
-        action_fig = plt.figure(1)
-        training_fig = plt.figure(2)
-        training_ax = training_fig.add_subplot(111)
-        training_ax.plot(avg_costs)
-        ax = action_fig.add_subplot(111, projection='3d')
-        ax.scatter(xs, ys, zs=pred_zs, c='b', label='pred')
-        ax.scatter(xs, ys, zs=zs, c='r', label='true')
-        plt.show()
-
+        nnc = NNControl()
+        # nnc.test(sess, avg_costs, action_pred, X)
+        nnc.run(sess, action_pred, X)
 
 if __name__ == "__main__":
     main()
