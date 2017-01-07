abb5a1e9fb9d6bacd5e15b5f0f5a074bd35e17f1
Peter Mitrano <mitranopeter@gmail.com>
2017-01-07 08:41:47 -0500

    renamed folder and implemented ravel/unravel

--- pendulum/q_learner.py
+++ pendulum/q_learner.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-import bloscpack as bp
-import time
-import os
 from math import sin, cos
-import sys
-import gym
-from gym import wrappers
 import numpy as np
 
 
@@ -21,11 +15,14 @@ class QLearner:
             self.state_dims.append(dim_n)
 
         self.action_bounds = action_bounds
-        self.action_n = (self.action_bounds[1] - self.action_bounds[0]) // self.action_bounds[2] + 1
+        self.action_dims = []
+        for bound in self.action_bounds:
+            dim_n = (bound[1] - bound[0]) // bound[2] + 1
+            self.action_dims.append(dim_n)
 
         self.epsilon = 1e-8
 
-        self.Q = np.random.rand(*self.state_dims, self.action_n)
+        self.Q = np.random.rand(*self.state_dims, *self.action_dims)
 
         self.lr = 1.0  # optimal for deterministic environment
         self.gamma = 0.9
@@ -35,34 +32,46 @@ class QLearner:
         self.Q = q_table
 
     def compute_state_idx(self, observation):
-        theta = np.arctan2(observation[1], observation[0])
-        theta = theta - self.min_angle
-        dtheta = observation[2] - self.min_dtheta
-        theta = (theta) // self.angle_step
-        dtheta = (dtheta) // self.dtheta_step
+        idxs = []
+        for dim in observation:
+            min_obs = self.state_bounds[0][0]
+            step_obs = self.state_bounds[0][2]
+            idxs.append((dim - min_obs) // step_obs)
 
-        # this is like flattening a 2d array
-        return int(theta + (self.angle_n * dtheta))
+        state_idx = np.ravel_multi_index(idxs, tuple(self.state_dims))
+        return state_idx
 
     def compute_action_from_idx(self, action_idx):
-        return action_idx * self.action_step + self.min_action
+        idxs = np.unravel_index(action_idx, tuple(self.action_dims))
+        action = []
+        for idx in idxs:
+            min_act = self.action_bounds[0][0]
+            step_act = self.action_bounds[0][2]
+            act = idx * step_act + min_act
+            action.append(act)
+
+        return action
 
     def _compute_state_from_idx(self, state_idx):
-        theta = (state_idx % self.angle_n) * self.angle_step + self.min_angle + self.epsilon
-        if theta - self.epsilon <= self.max_angle < theta:
-            theta -= self.epsilon
-        dtheta = (state_idx // self.angle_n) * self.dtheta_step + self.min_dtheta + self.epsilon
-        if dtheta - self.epsilon <= self.max_dtheta < dtheta:
-            theta -= self.epsilon
-        return [cos(theta), sin(theta), dtheta]
+        idxs = np.unravel_index(state_idx, tuple(self.state_dims))
+        state = []
+        for idx in idxs:
+            min_st = self.state_bounds[0][0]
+            step_st = self.state_bounds[0][2]
+            st = idx * step_st + min_st
+            state.append(st)
+
+        return state
 
     def _compute_action_idx(self, action):
-        if action > self.max_action:
-            raise ValueError("Action value %f is greater than max of %f", action, self.max_action)
+        idxs = []
+        for dim in action:
+            min_obs = self.action_bounds[0][0]
+            step_obs = self.action_bounds[0][2]
+            idxs.append((dim - min_obs) // step_obs)
 
-        action_idx = (action - self.min_action + self.epsilon) // self.action_step
-        action_idx = min(action_idx, 19)
-        return int(action_idx)
+        action_idx = np.ravel_multi_index(idxs, tuple(self.action_dims))
+        return action_idx
 
     def q_policy(self, observation, noise_level=1):
         state_idx = self.compute_state_idx(observation)
--- robot_temp/utree_robot_temp.py
+++ robot_temp/utree_robot_temp.py
@@ -8,15 +8,39 @@ if __name__ == "__main__":
 
     tree = ContinuousUTree(2, 6)
 
-    for i in range(100):
+    # train
+    for i in range(1000):
         obs = env.reset()
         j = 0
         while True:
             move = int(random.random() + 0.5)
             temp = random.randint(0, 2)
             # env.render()
-            obs_prime, reward, done, info = env.step([move, temp])
             action_idx = numpy.ravel_multi_index(([move, temp]), (2, 3))
+            obs_prime, reward, done, info = env.step([move, temp])
+            tree.add_transition(obs, action_idx, obs_prime, reward)
+            # print(obs, reward)
+            obs = obs_prime
+
+            j += 1
+            if j > 200:
+                break
+
+            if done:
+                tree.process()
+                print("success!")
+                break
+
+    # test
+    for i in range(10):
+        obs = env.reset()
+        j = 0
+        while True:
+            env.render()
+
+            action_idx = tree.best_action_idx(obs)
+            move, temp = numpy.unravel_index(action_idx, (2, 3))
+            obs_prime, reward, done, info = env.step([move, temp])
             tree.add_transition(obs, action_idx, obs_prime, reward)
             # print(obs, reward)
             obs = obs_prime
