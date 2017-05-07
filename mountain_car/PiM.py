d72bdef11cb3c9007d8f1586538606250248a084
Peter Mitrano <mitranopeter@gmail.com>
2017-05-07 14:36:39 -0400

    hand-coded model parameters seem to work

--- mountain_car/PiM.py
+++ mountain_car/PiM.py
@@ -17,93 +17,91 @@ class PolicyInModel:
     def __init__(self):
         self.state_dim = 2
         self.state_bounds = np.array([[-1.2, 0.6], [-0.07, 0.07]])
+        self.state_sizes = self.state_bounds[:, 1] - self.state_bounds[:, 0]
         self.action_dim = 3
+        self.on_policy_learning = False
 
         with tf.name_scope("fed_values"):
             self.state = tf.placeholder(tf.float32, shape=[1, self.state_dim], name='state')
             self.true_next_state = tf.placeholder(tf.float32, shape=[1, self.state_dim], name='state_next')
             self.manual_action = tf.placeholder(tf.int32, shape=[], name='manual_action')
             self.reward = tf.placeholder(tf.int32, shape=[], name='reward')
-            tf.summary.scalar("position", self.state[0][0])
-            tf.summary.scalar("velocity", self.state[0][1])
+            tf.summary.scalar("true_next_position", self.true_next_state[0][0])
+            tf.summary.scalar("true_next_velocity", self.true_next_state[0][1])
             tf.summary.scalar("reward", self.reward)
 
-        with tf.name_scope('policy'):
-            self.policy_h1_dim = 5
-            self.policy_w1 = tf.Variable(tf.truncated_normal([self.state_dim, self.policy_h1_dim], 0, 0.1),
-                                         name='policy_w1')
-            self.policy_b1 = tf.Variable(tf.constant(0.1, shape=[self.policy_h1_dim]), name='policy_b1')
-            self.policy_h1 = tf.nn.relu(tf.matmul(self.state, self.policy_w1, name='matmul1') + self.policy_b1,
-                                        name='relu')
+        if self.on_policy_learning:
+            with tf.name_scope('policy'):
+                self.policy_h1_dim = 5
+                self.policy_w1 = tf.Variable(tf.truncated_normal([self.state_dim, self.policy_h1_dim], 0, 0.1),
+                                             name='policy_w1')
+                self.policy_b1 = tf.Variable(tf.constant(0.1, shape=[self.policy_h1_dim]), name='policy_b1')
+                self.policy_h1 = tf.nn.relu(tf.matmul(self.state, self.policy_w1, name='matmul1') + self.policy_b1,
+                                            name='relu')
 
-            self.policy_w2 = tf.Variable(tf.truncated_normal((self.policy_h1_dim, self.action_dim), 0, 0.1),
-                                         name='policy_w2')
-            self.policy_b2 = tf.Variable(tf.constant(0.1, shape=[self.action_dim]), name='policy_b2')
-            self.policy_action_float = tf.nn.softmax(
-                tf.matmul(self.policy_h1, self.policy_w2, name='matmul1') + self.policy_b2)
-            self.policy_action = tf.argmax(self.policy_action_float, axis=1)[0]
-            self.policy_vars = [self.policy_w1, self.policy_b1, self.policy_w2, self.policy_b2]
+                self.policy_w2 = tf.Variable(tf.truncated_normal((self.policy_h1_dim, self.action_dim), 0, 0.1),
+                                             name='policy_w2')
+                self.policy_b2 = tf.Variable(tf.constant(0.1, shape=[self.action_dim]), name='policy_b2')
+                self.policy_action_float = tf.nn.softmax(
+                    tf.matmul(self.policy_h1, self.policy_w2, name='matmul1') + self.policy_b2)
+                self.policy_action = tf.argmax(self.policy_action_float, axis=1)[0]
+                self.policy_vars = [self.policy_w1, self.policy_b1, self.policy_w2, self.policy_b2]
 
-            tf.summary.histogram('policy_w1', self.policy_w1)
-            tf.summary.histogram('policy_b1', self.policy_b1)
-            tf.summary.histogram('policy_w2', self.policy_w2)
-            tf.summary.histogram('policy_b2', self.policy_b2)
+                tf.summary.histogram('policy_w1', self.policy_w1)
+                tf.summary.histogram('policy_b1', self.policy_b1)
+                tf.summary.histogram('policy_w2', self.policy_w2)
+                tf.summary.histogram('policy_b2', self.policy_b2)
 
         with tf.name_scope("action"):
-            tf.summary.scalar("policy_action", self.policy_action)
+            if self.on_policy_learning:
+                tf.summary.scalar("policy_action", self.policy_action)
             tf.summary.scalar("manual_action", self.manual_action)
 
         with tf.name_scope('model'):
-            self.model_input = tf.concat((self.policy_action_float, self.state), axis=1, name='concat')
-            # self.manual_action_float = tf.expand_dims(
-            #     tf.one_hot(self.manual_action, self.action_dim, dtype=tf.float32,
-            #                name='manual_action_float'), axis=0)
-            # self.model_input = tf.concat((self.manual_action_float, self.state), axis=1, name='concat')
+            if self.on_policy_learning:
+                self.model_input = tf.concat((self.state, self.policy_action_float), axis=1, name='concat')
+            else:
+                self.manual_action_float = tf.expand_dims(
+                    tf.one_hot(self.manual_action, self.action_dim, dtype=tf.float32, name='manual_action_float'),
+                    axis=0)
+                self.model_input = tf.concat((self.state, self.manual_action_float), axis=1, name='concat')
 
-            self.model_h1_dim = 4
-            self.model_w1 = tf.Variable(
-                tf.truncated_normal([self.state_dim + self.action_dim, self.model_h1_dim], 0, 0.1),
-                name='model_w1')
-            self.model_b1 = tf.Variable(tf.constant(0.1, shape=[self.model_h1_dim]), name='model_b1')
-            self.model_h1 = tf.nn.relu(tf.matmul(self.model_input, self.model_w1) + self.model_b1)
+            self.model_w1 = tf.Variable([[-.0075, 1], [1, 0], [-0.001, 0], [0, 0], [0.001, 0]], name='model_w1')
+            self.predicted_next_state = tf.matmul(self.model_input, self.model_w1)
+            self.model_vars = [self.model_w1]
 
-            self.model_w2 = tf.Variable(tf.truncated_normal((self.model_h1_dim, self.state_dim), 0, 0.1),
-                                        name='model_w2')
-            self.model_b2 = tf.Variable(tf.constant(0.1, shape=[self.state_dim]), name='model_b2')
-            self.model_raw_state = tf.nn.sigmoid(tf.matmul(self.model_h1, self.model_w2) + self.model_b2,
-                                                 name='model_norm')
-            self.predicted_next_state = self.model_raw_state * (
-                self.state_bounds[:, 1] - self.state_bounds[:, 0]) + self.state_bounds[:, 0]
-            self.model_vars = [self.model_w1, self.model_b1, self.model_w2, self.model_b2]
-
-            tf.summary.histogram('next_state', self.predicted_next_state)
-            tf.summary.histogram('true_next_state', self.true_next_state)
+            tf.summary.scalar("predicted_position", self.predicted_next_state[0][0])
+            tf.summary.scalar("predicted_velocity", self.predicted_next_state[0][1])
 
         with tf.name_scope("loss"):
             self.state_change = self.predicted_next_state - self.state
             self.policy_loss = -tf.nn.l2_loss(self.state_change, name='policy_loss')
-            self.model_loss = tf.nn.l2_loss(self.predicted_next_state - self.true_next_state, name='model_loss')
+            self.model_loss = tf.nn.l2_loss((self.predicted_next_state - self.true_next_state) / self.state_sizes,
+                                            name='model_loss')
             tf.summary.histogram("state_change", tf.abs(self.state_change))
             tf.summary.scalar("policy_loss", self.policy_loss)
             tf.summary.scalar("model_loss", self.model_loss)
 
-        with tf.name_scope("policy_gradients"):
-            grads = list(zip(tf.gradients(self.policy_loss, self.policy_vars), self.policy_vars))
-            for grad, var in grads:
-                tf.summary.histogram(var.name + '/gradient', grad)
+        if self.on_policy_learning:
+            with tf.name_scope("policy_gradients"):
+                grads = list(zip(tf.gradients(self.policy_loss, self.policy_vars), self.policy_vars))
+                for grad, var in grads:
+                    tf.summary.histogram(var.name + '/gradient', grad)
 
         with tf.name_scope("model_gradients"):
             grads = list(zip(tf.gradients(self.model_loss, self.model_vars), self.model_vars))
             for grad, var in grads:
                 tf.summary.histogram(var.name + '/gradient', grad)
 
-        self.learning_rate = 0.01
-        self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
+        self.initial_learning_rate = 0.1
         self.global_step = tf.Variable(0, trainable=False)
-        self.train_policy = self.optimizer.minimize(self.policy_loss, var_list=self.policy_vars,
-                                                    global_step=self.global_step)
+        self.learning_rate = tf.train.exponential_decay(self.initial_learning_rate, self.global_step, 1000, 0.9)
+        self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
         self.train_model = self.optimizer.minimize(self.model_loss, var_list=self.model_vars,
                                                    global_step=self.global_step)
+        if self.on_policy_learning:
+            self.train_policy = self.optimizer.minimize(self.policy_loss, var_list=self.policy_vars,
+                                                        global_step=self.global_step)
 
         self.init = tf.global_variables_initializer()
 
@@ -131,7 +129,7 @@ class PolicyInModel:
             tb_writer.add_graph(sess.graph)
             sess.run(self.init)
 
-            for i in range(500):
+            for i in range(1500):
                 episode_iters = 0
                 total_reward = 0
                 observation = env.reset()
@@ -140,28 +138,31 @@ class PolicyInModel:
 
                 while episode_iters < 400:
 
-                    feed_dict = {self.state: [observation], self.true_next_state: [next_observation],
-                                 self.manual_action: action, self.reward: total_reward}
-                    _, m_loss, next_state, action = sess.run(
-                        [self.train_model, self.model_loss, self.predicted_next_state, self.policy_action],
-                        feed_dict)
-                    _, p_loss = sess.run([self.train_policy, self.policy_loss], feed_dict)
+                    feed_dict = {
+                        self.state: [observation],
+                        self.true_next_state: [next_observation],
+                        self.manual_action: action,
+                        self.reward: total_reward
+                    }
 
+                    m_loss, next_state = sess.run([self.model_loss, self.predicted_next_state],
+                                                     feed_dict)
+                    if self.on_policy_learning:
+                        _, p_loss, action = sess.run([self.train_policy, self.policy_loss, self.policy_action],
+                                                     feed_dict)
+
+                    print('%f %f %f %f' % (next_state[0][1], next_state[0][0], next_observation[0], next_observation[1]))
                     # comment this back in for off-policy learning
-                    # speed = observation[1]
-                    #
-                    # if speed < 0:
-                    #     action = 0
-                    # else:
-                    #     action = 2
+                    speed = observation[1]
+
+                    if speed < 0:
+                        action = 0
+                    else:
+                        action = 2
 
                     summary, step = sess.run([self.merged_summary, self.global_step], feed_dict)
                     tb_writer.add_summary(summary, step)
 
-                    if i == 200:
-                        print(sess.run(self.state_change, feed_dict), action)
-
-                    # print(action)
                     observation = next_observation
                     next_observation, reward, done, info = env.step(action)
                     total_reward += reward
@@ -171,8 +172,6 @@ class PolicyInModel:
 
                     if done:
                         break
-
-            print(sess.run([self.model_w1, self.model_b1, self.model_w2, self.model_b2]))
 
         if upload:
             env.close()
