dd73b5168c0990474a3450dff884cd6c80f0de90
Peter Mitrano <mitranopeter@gmail.com>
2017-05-09 01:53:51 -0400

    trying on policy, but not learning
    just using hard coded values
    
    tensorboard log-dir: log_data/May_09/May_09_01:53:51/

--- mountain_car/PiM.py
+++ mountain_car/PiM.py
@@ -20,8 +20,8 @@ class PolicyInModel:
         self.state_sizes = self.state_bounds[:, 1] - self.state_bounds[:, 0]
         self.action_dim = 3
         self.episode_max_iters = 400
-        # self.on_policy_learning = True
-        self.on_policy_learning = False
+        self.on_policy_learning = True
+        # self.on_policy_learning = False
 
         with tf.name_scope("fed_values"):
             self.state = tf.placeholder(tf.float32, shape=[1, self.state_dim], name='state')
@@ -47,8 +47,9 @@ class PolicyInModel:
                 # self.policy_action_float = tf.nn.softmax(tf.matmul(self.policy_h1, self.policy_w2, name='matmul1') + self.policy_b2)
 
                 self.policy_w1 = tf.Variable([[0, 0, 0], [-1, 0, 1.]], name='policy_w1')
-                self.policy_action_float = tf.nn.softmax(tf.matmul(self.state, self.policy_w1, name='matmul1'))
+                self.policy_action_float = tf.matmul(self.state, self.policy_w1, name='matmul1')
                 self.policy_action = tf.argmax(self.policy_action_float, axis=1)[0]
+                self.policy_action_one_hot = tf.expand_dims(tf.one_hot(self.policy_action, self.action_dim, name='action_one_hot'), axis=0)
                 self.policy_vars = [self.policy_w1]
 
                 tf.summary.histogram('policy_w1', self.policy_w1)
@@ -64,20 +65,16 @@ class PolicyInModel:
 
         with tf.name_scope('model'):
             if self.on_policy_learning:
-                self.model_input = tf.concat((self.state, self.policy_action_float), axis=1, name='concat')
+                self.model_input = tf.concat((self.state, self.policy_action_one_hot), axis=1, name='concat')
             else:
-                self.manual_action_one_hot = tf.expand_dims(
-                    tf.one_hot(self.manual_action, self.action_dim, dtype=tf.float32, name='manual_action_float'),
-                    axis=0)
+                self.manual_action_one_hot = tf.expand_dims(tf.one_hot(self.manual_action, self.action_dim, dtype=tf.float32, name='manual_action_float'), axis=0)
                 self.model_input = tf.concat((self.state, self.manual_action_one_hot), axis=1, name='concat')
 
             self.model_h1_dim = 10
-            self.model_w1 = tf.Variable(
-                tf.truncated_normal([self.state_dim + self.action_dim, self.model_h1_dim], 0, 0.01), name='model_w1')
+            self.model_w1 = tf.Variable(tf.truncated_normal([self.state_dim + self.action_dim, self.model_h1_dim], 0, 0.01), name='model_w1')
             self.model_b1 = tf.Variable(tf.constant(0.1, shape=[self.model_h1_dim]), name='model_b1')
             self.model_h1 = tf.nn.relu(tf.matmul(self.model_input, self.model_w1) + self.model_b1)
-            self.model_w2 = tf.Variable(tf.truncated_normal([self.model_h1_dim, self.state_dim], 0, 0.01),
-                                        name='model_w2')
+            self.model_w2 = tf.Variable(tf.truncated_normal([self.model_h1_dim, self.state_dim], 0, 0.01), name='model_w2')
             self.model_b2 = tf.Variable(tf.constant(0.1, shape=[self.state_dim]), name='model_b2')
             self.predicted_next_state = tf.matmul(self.model_h1, self.model_w2) + self.model_b2
             self.model_vars = [self.model_w1]
@@ -100,11 +97,11 @@ class PolicyInModel:
             tf.summary.scalar("policy_loss", self.policy_loss)
             tf.summary.scalar("model_loss", self.model_loss)
 
-        if self.on_policy_learning:
-            with tf.name_scope("policy_gradients"):
-                grads = list(zip(tf.gradients(self.policy_loss, self.policy_vars), self.policy_vars))
-                for grad, var in grads:
-                    tf.summary.histogram(var.name + '/gradient', grad)
+        # if self.on_policy_learning:
+        #     with tf.name_scope("policy_gradients"):
+        #         grads = list(zip(tf.gradients(self.policy_loss, self.policy_vars), self.policy_vars))
+        #         for grad, var in grads:
+        #             tf.summary.histogram(var.name + '/gradient', grad)
 
         with tf.name_scope("model_gradients"):
             grads = list(zip(tf.gradients(self.model_loss, self.model_vars), self.model_vars))
@@ -119,9 +116,9 @@ class PolicyInModel:
         self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
         self.train_model = self.optimizer.minimize(self.model_loss, var_list=self.model_vars,
                                                    global_step=self.global_step)
-        if self.on_policy_learning:
-            self.train_policy = self.optimizer.minimize(self.policy_loss, var_list=self.policy_vars,
-                                                        global_step=self.global_step)
+        # if self.on_policy_learning:
+        #     self.train_policy = self.optimizer.minimize(self.policy_loss, var_list=self.policy_vars,
+        #                                                 global_step=self.global_step)
 
         self.init = tf.global_variables_initializer()
         tf.summary.scalar("learning_rate", self.learning_rate)
@@ -189,8 +186,7 @@ class PolicyInModel:
                         else:
                             action = 2
 
-                        # FIXME
-                        # action = np.random.randint(0, self.action_dim)
+                            # action = np.random.randint(0, self.action_dim)
 
                     if episode_iters % 5 == 0:
                         summary, step = sess.run([self.merged_summary, self.global_step], feed_dict)
