4c61433c5ba63e881d8ad4e5878c73962282771d
Peter Mitrano <mitranopeter@gmail.com>
2017-05-10 00:04:07 -0400

    manual weights model position well, not sure if it models velocity well
    it's hard to tell from tensorboard graphs
    so a good "baseline" model loss might be 1e-3 or 1e-2
    trying now with learned model, but actually allow all weights to train now
    
    tensorboard log-dir: log_data/May_10/May_10_00:04:07/

--- mountain_car/PiM.py
+++ mountain_car/PiM.py
@@ -77,33 +77,32 @@ class PolicyInModel:
                 self.model_input = tf.concat((self.state, self.manual_action_one_hot), axis=1, name='concat')
 
             # hard coded shit
-            self.model_w1 = tf.Variable([[1.00, -0.0025], [1, 0], [-0.001, 0], [0, 0], [0.001, 0]], name='model_w1')
-            self.predicted_next_state = tf.matmul(self.model_input, self.model_w1)
-            self.model_vars = [self.model_w1]
+            # self.model_w1 = tf.Variable([[1.00, -0.0025], [1, 0], [-0.001, 0], [0, 0], [0.001, 0]], name='model_w1')
+            # self.predicted_next_state = tf.matmul(self.model_input, self.model_w1)
+            # self.model_vars = [self.model_w1]
 
             # fancy model
-            # self.model_h1_dim = 10
-            # self.model_w1 = tf.Variable(
-            #     tf.truncated_normal([self.state_dim + self.action_dim, self.model_h1_dim], 0, 0.01), name='model_w1')
-            # self.model_b1 = tf.Variable(tf.constant(0.1, shape=[self.model_h1_dim]), name='model_b1')
-            # self.model_h1 = tf.nn.relu(tf.matmul(self.model_input, self.model_w1) + self.model_b1)
-            # self.model_w2 = tf.Variable(tf.truncated_normal([self.model_h1_dim, self.state_dim], 0, 0.01),
-            #                             name='model_w2')
-            # self.model_b2 = tf.Variable(tf.constant(0.1, shape=[self.state_dim]), name='model_b2')
-            # self.predicted_next_state = tf.matmul(self.model_h1, self.model_w2) + self.model_b2
-            # self.model_vars = [self.model_w1, self.model_b1, self.model_w2, self.model_b2]
+            self.model_h1_dim = 10
+            self.model_w1 = tf.Variable(
+                tf.truncated_normal([self.state_dim + self.action_dim, self.model_h1_dim], 0, 0.01), name='model_w1')
+            self.model_b1 = tf.Variable(tf.constant(0.1, shape=[self.model_h1_dim]), name='model_b1')
+            self.model_h1 = tf.nn.relu(tf.matmul(self.model_input, self.model_w1) + self.model_b1)
+            self.model_w2 = tf.Variable(tf.truncated_normal([self.model_h1_dim, self.state_dim], 0, 0.01),
+                                        name='model_w2')
+            self.model_b2 = tf.Variable(tf.constant(0.1, shape=[self.state_dim]), name='model_b2')
+            self.predicted_next_state = tf.matmul(self.model_h1, self.model_w2) + self.model_b2
+            self.model_vars = [self.model_w1, self.model_b1, self.model_w2, self.model_b2]
 
             tf.summary.histogram("model_w1", self.model_w1)
-            # tf.summary.histogram("model_b1", self.model_b1)
-            # tf.summary.histogram("model_w2", self.model_w2)
-            # tf.summary.histogram("model_b2", self.model_b2)
+            tf.summary.histogram("model_b1", self.model_b1)
+            tf.summary.histogram("model_w2", self.model_w2)
+            tf.summary.histogram("model_b2", self.model_b2)
             tf.summary.scalar("predicted_position", self.predicted_next_state[0][0])
             tf.summary.scalar("predicted_velocity", self.predicted_next_state[0][1])
 
         with tf.name_scope("loss"):
-            # self.state_change = self.predicted_next_state - self.state
-            # self.policy_loss = -tf.nn.l2_loss(self.state_change, name='policy_loss')
-            self.policy_loss = 1 + self.predicted_next_state[0][0]
+            self.state_change = self.predicted_next_state - self.state
+            self.policy_loss = -tf.nn.l2_loss(self.state_change, name='policy_loss')
             alpha = 1e-3
             self.model_loss = tf.nn.l2_loss((self.predicted_next_state - self.true_next_state) / self.state_sizes,
                                             name='model_loss') + alpha * tf.nn.l2_loss(self.model_w1)
@@ -188,8 +187,8 @@ class PolicyInModel:
                         self.reward: total_reward
                     }
 
-                    # _, m_loss, next_state = sess.run([self.train_model, self.model_loss, self.predicted_next_state], feed_dict)
-                    m_loss, next_state = sess.run([self.model_loss, self.predicted_next_state], feed_dict)
+                    _, m_loss, next_state = sess.run([self.train_model, self.model_loss, self.predicted_next_state], feed_dict)
+                    # m_loss, next_state = sess.run([self.model_loss, self.predicted_next_state], feed_dict)
                     if self.on_policy_learning:
                         p_loss, action = sess.run([self.policy_loss, self.policy_action],
                                                   feed_dict)
@@ -208,7 +207,7 @@ class PolicyInModel:
                         else:
                             action = np.random.randint(0, self.action_dim)
 
-                    if episode_iters % 5 == 0:
+                    if episode_iters % 10 == 0:
                         summary, step = sess.run([self.merged_summary, self.global_step], feed_dict)
                         tb_writer.add_summary(summary, step)
 
